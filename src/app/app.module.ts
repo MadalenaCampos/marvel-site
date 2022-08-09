97a87749e5b341ea4742a7f69e91f236fd00d027
MadalenaCampos <madacampos02@gmail.com>
2022-08-09 16:59:45 -0300

    :sparkles: cricao da home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { registerLocaleData } from '@angular/common';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { AppComponent } from './app.component';
+import { HomeModule } from './pages/home/home.module';
+
 import { NZ_I18N } from 'ng-zorro-antd/i18n';
 import { en_US } from 'ng-zorro-antd/i18n';
-import { registerLocaleData } from '@angular/common';
 import en from '@angular/common/locales/en';
-import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+registerLocaleData(en);
 
 // Módulos do NgZorro
 import { NzButtonModule } from 'ng-zorro-antd/button';
@@ -25,8 +28,6 @@ import {
 
 const icons: IconDefinition[] = [AccountBookFill, AlertOutline, AlertFill];
 
-registerLocaleData(en);
-
 @NgModule({
   declarations: [AppComponent],
   imports: [
@@ -35,11 +36,11 @@ registerLocaleData(en);
     FormsModule,
     HttpClientModule,
     BrowserAnimationsModule,
+    HomeModule,
 
     // Módulos do NgZorro
     NzIconModule,
     NzButtonModule,
-
     // Solução para problema com ícones do NG-ZORRO
     NzIconModule.forRoot(icons),
   ],

294c91a968cd74e11140d9730da3691e06ae7fb2
MadalenaCampos <madacampos02@gmail.com>
2022-08-09 10:44:31 -0300

    :tada: iniciando projeto e instalando o zorro

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,41 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NZ_I18N } from 'ng-zorro-antd/i18n';
+import { en_US } from 'ng-zorro-antd/i18n';
+import { registerLocaleData } from '@angular/common';
+import en from '@angular/common/locales/en';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+// Modulos do NgZorro
+import { NzIconModule } from 'ng-zorro-antd/icon';
+
+// Solução para problema com ícones do NG-ZORRO
+import { IconDefinition } from '@ant-design/icons-angular';
+import {
+  AccountBookFill,
+  AlertFill,
+  AlertOutline,
+} from '@ant-design/icons-angular/icons';
+
+const icons: IconDefinition[] = [AccountBookFill, AlertOutline, AlertFill];
+
+registerLocaleData(en);
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    NzIconModule,
+    NzIconModule.forRoot(icons),
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [{ provide: NZ_I18N, useValue: en_US }],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}

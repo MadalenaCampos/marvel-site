7fff7e9a5abc40ea31a010896de1be4441ac899b
MadalenaCampos <madacampos02@gmail.com>
2022-08-11 16:04:38 -0300

    :construction: criando estrutura exemplo dos cards de personagens

--- src/app/pages/characters/characters.module.ts
+++ src/app/pages/characters/characters.module.ts
@@ -5,6 +5,7 @@ import { CharactersComponent } from './characters.component';
 
 import { NzInputModule } from 'ng-zorro-antd/input';
 import { NzAutocompleteModule } from 'ng-zorro-antd/auto-complete';
+import { NzButtonModule } from 'ng-zorro-antd/button';
 
 import { NzIconModule } from 'ng-zorro-antd/icon';
 import { IconDefinition } from '@ant-design/icons-angular';
@@ -24,6 +25,7 @@ const icons: IconDefinition[] = [AccountBookFill, AlertOutline, AlertFill];
     NzAutocompleteModule,
     NzIconModule,
     NzIconModule.forRoot(icons),
+    NzButtonModule
   ],
 })
 export class CharactersModule {}

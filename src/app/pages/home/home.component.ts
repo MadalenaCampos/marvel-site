10c6ff89dc22e7235a7bece5dd3393a88471248b
MadalenaCampos <madacampos02@gmail.com>
2022-09-12 11:51:11 -0300

    :art: mudancas de designe na home

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { NzMessageService } from 'ng-zorro-antd/message';
+import { NzModalRef, NzModalService } from 'ng-zorro-antd/modal';
+import { CharacterDetailComponent } from 'src/app/modals/character-detail/character-detail.component';
+import { CharactersService } from 'src/app/services/characters/characters.service';
+
+interface Character {
+  id: number;
+  name: string;
+  description: string;
+  thumbnail: {
+    path: string;
+    extension: string;
+  };
+}
 
 @Component({
   selector: 'app-home',
@@ -6,7 +21,54 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.less'],
 })
 export class HomeComponent implements OnInit {
-  constructor() {}
+  public characters!: any;
+  public modalCharacter!: NzModalRef;
 
-  ngOnInit(): void {}
+  constructor(
+    private router: Router,
+    private charactersService: CharactersService,
+    private modalService: NzModalService,
+    private message: NzMessageService
+  ) {}
+
+  public getPersonagens() {
+    const offsetAleatorio = Math.floor(Math.random() * 520);
+
+    this.charactersService.getAllCharacters(3, offsetAleatorio).subscribe(
+      ({ data }) => {
+        this.characters = data.results;
+      },
+      ({ error }) => {
+        this.message.error(error.code + ' - ' + error.status);
+      }
+    );
+  }
+
+  public openModal(nzContent: any, character?: Character): void {
+    this.modalCharacter = this.modalService.create({
+      nzContent,
+      nzFooter: null,
+      nzWidth: '740px',
+      nzMaskClosable: false,
+      nzStyle: {
+        'box-shadow': '0px 0px 8px rgba(0, 0, 0, 0.2)',
+        'border-radius': '22px',
+      },
+      nzComponentParams: {
+        character,
+      },
+    });
+  }
+
+  public getInformacoesDoPersoangem(character: Character): void {
+    this.openModal(CharacterDetailComponent, character);
+  }
+
+  public irParaPersonagens() {
+    this.router.navigate(['/characters']);
+  }
+
+  ngOnInit(): void {
+    this.getPersonagens();
+  }
 }
--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
+import { NzToolTipModule } from 'ng-zorro-antd/tooltip';
+import { NzButtonModule } from 'ng-zorro-antd/button';
 
 @NgModule({
   declarations: [HomeComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, NzToolTipModule, NzButtonModule],
   exports: [HomeComponent],
 })
 export class HomeModule {}
